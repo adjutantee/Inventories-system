db4c85b303f2b624ef862c2f6d494cf75f5d0362
Izagakhmaev Ramazan <78782750+adjutantee@users.noreply.github.com>
2023-02-14 16:09:47 +0300

    save

---
+++ Inventories.Services.xUnit/Common/InventoriesContextFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventories.Services.xUnit.Common
+{
+    internal class InventoriesContextFactory
+    {
+    }
+}
---
+++ Inventories.Services.xUnit/Inventories/Commands/CreateInventoriesCommandHandlerTests.cs
@@ -0,0 +1,15 @@
+﻿using Inventories.Services.xUnit.Common;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+namespace Inventories.Services.xUnit.Inventories.Commands
+{
+    public class CreateInventoriesCommandHandlerTests : TestCommandBase
+    {
+        [Fact]
+        public async Task CreateInventoriesCommandHandler_Success()
+        {
+            var handler = new CreateInventoriesCommandHandler(customerContext);
+        }
+    }
+}
