e0c8fb7339e53b2d7862af1e1ad921e0a862cdf9
Izagakhmaev Ramazan <78782750+adjutantee@users.noreply.github.com>
2023-02-15 15:02:34 +0300

    Add SD

--- Inventories.Services.IdentityCore/SD.cs
+++ Inventories.Services.IdentityCore/SD.cs
@@ -1,6 +1,22 @@
-﻿namespace Inventories.Services.IdentityCore
+﻿using Duende.IdentityServer.Models;
+
+namespace Inventories.Services.IdentityCore
 {
-    public class SD
+    public static class SD
     {
+        public const string AdminUser = "Админ";
+        public const string CustomerUser = "Управление поставщиками";
+        public const string ItemsUser = "Управление товарами";
+        public const string OrdersUser = "Управление накладными";
+        public const string StockUser = "Управление складом";
+        public const string WarehouseUser = "Управление перемещением товаров";
+
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Email(),
+                new IdentityResources.Profile()
+            };
     }
 }
